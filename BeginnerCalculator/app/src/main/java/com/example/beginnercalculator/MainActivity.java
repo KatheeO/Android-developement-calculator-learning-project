951abd2970e475b022a8feb5328d2bb752e1bbff
Kaitlyn <44295069+KatheeO@users.noreply.github.com>
2021-10-12 09:04:45 +0000

    Update MainActivity.java (#4)

--- BeginnerCalculator/app/src/main/java/com/example/beginnercalculator/MainActivity.java
+++ BeginnerCalculator/app/src/main/java/com/example/beginnercalculator/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.beginnercalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnEq = findViewById(R.id.btnEq);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
